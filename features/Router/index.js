2d14a94492df60dbba70c38557339ff0fd246d9a
Jim Le <jim@height.io>
2021-11-28 16:47:56 +0000

    fix where hashchanges happen before data is ready, throwing exception

--- features/Router/index.js
+++ features/Router/index.js
@@ -1,7 +1,8 @@
 import isNil from 'https://cdn.skypack.dev/lodash.isnil';
 import { fromEvent, asyncScheduler } from 'https://cdn.skypack.dev/rxjs';
 import { map, startWith, distinctUntilChanged, observeOn, share } from 'https://cdn.skypack.dev/rxjs/operators';
-import { dispatch } from '../../actions/index.js';
+import { ofType, dispatch } from '../../actions/index.js';
+import { ACTIONS as ACTIONS_DATASETS } from '../../datasets/index.js';
 import { PageNotFoundError } from './errors.js';
 
 export const ACTIONS = {
@@ -15,27 +16,29 @@ export const ALLOW_LIST = [
 /**
  * auto listen to window hash changes
  */
-export const hashChangeObservable = fromEvent(window, 'hashchange')
-  .pipe(
-    startWith(window.location.hash),
-    map(() => window.location.hash),
-    distinctUntilChanged(),
-    map(hash => {
-      const [page, ...params] = hash.slice(1, hash.length).split('/');
-      const event = { type: ACTIONS.NAVIGATE_TO, payload: { page, params }, error: null };
+ofType(ACTIONS_DATASETS.DATA_READY)
+  .subscribe(async () => {
+    fromEvent(window, 'hashchange')
+      .pipe(
+        startWith(window.location.hash),
+        map(() => window.location.hash),
+        distinctUntilChanged(),
+        map(hash => {
+          const [page, ...params] = hash.slice(1, hash.length).split('/');
+          const event = { type: ACTIONS.NAVIGATE_TO, payload: { page, params }, error: null };
 
-      const isValidPageParams = !isNil(page) && ALLOW_LIST.includes(page);
-      if (!isValidPageParams) {
-        event.error = new PageNotFoundError(`Expected one of "${ALLOW_LIST.join(',')}", got "${page}"`);
-      }
-      return event;
-    }),
-    observeOn(asyncScheduler),
-    share()
-  )
-  .subscribe(event => {
-    dispatch(event);
-  })
-
+          const isValidPageParams = !isNil(page) && ALLOW_LIST.includes(page);
+          if (!isValidPageParams) {
+            event.error = new PageNotFoundError(`Expected one of "${ALLOW_LIST.join(',')}", got "${page}"`);
+          }
+          return event;
+        }),
+        observeOn(asyncScheduler),
+        share()
+      )
+      .subscribe(event => {
+        dispatch(event);
+      })
+  });
 
 export default null;
